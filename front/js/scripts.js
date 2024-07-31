77adb2f3bfad8886ca87944aa1428081e2e1ad34
TatisLesmes <tatis26lesmesmail.com>
2024-07-30 21:05:40 -0500

    cambio

---
+++ front/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#search-form').submit(function(event) {
+        event.preventDefault();
+        // Aquí puedes hacer la llamada a la API para obtener los datos y actualizar el DOM
+    });
+});
