10fef2e7734d45a661a40d26689946cb9e6e2b38
Lauragranados2021 <laura.granados05@uptc.edu.co>
2024-07-31 01:22:13 -0500

    summary hecho

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -20,11 +20,11 @@ const BuscarCedula = () => {
                 detailsContainer.appendChild(destinoLabel);
     
                 const fechaLabel = document.createElement('p');
-                fechaLabel.textContent = `Fecha: ${practica.fecha}`;
+                fechaLabel.textContent = `Fecha Inicio: ${practica.inicio}`;
                 detailsContainer.appendChild(fechaLabel);
     
                 const periodoLabel = document.createElement('p');
-                periodoLabel.textContent = `Periodo: ${practica.periodo}`;
+                periodoLabel.textContent = `Fecha Fin: ${practica.fin}`;
                 detailsContainer.appendChild(periodoLabel);
 
                 const empresaLabel = document.createElement('p');
@@ -54,7 +54,67 @@ const BuscarCedula = () => {
     });
 
 }
+function formatDateToISO(date) {
+    return new Date(date).toISOString();
+}
+const cargarResumen= () => {
+    const periodoInicio = document.getElementById('periodoInicio').value;
+    const periodoFin = document.getElementById('periodoFin').value;
+    const startDate = formatDateToISO(periodoInicio);
+    const endDate = formatDateToISO(periodoFin);
+    fetch(`http://localhost:8080/practica/dateRange/${startDate}/${endDate}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
 
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        console.log(responseData);
+        const detailsContainer = document.getElementById('detailsContainer');
+        detailsContainer.innerHTML = ''; // Clear previous details
+    
+        if (responseData.status === 'FOUND' && Array.isArray(responseData.data)) {
+            responseData.data.forEach(practica => {
+                const destinoLabel = document.createElement('div');
+                destinoLabel.textContent = `Destino: ${practica.destino}`;
+                detailsContainer.appendChild(destinoLabel);
+    
+                const fechaLabel = document.createElement('p');
+                fechaLabel.textContent = `Fecha Inicio: ${practica.inicio}`;
+                detailsContainer.appendChild(fechaLabel);
+    
+                const periodoLabel = document.createElement('p');
+                periodoLabel.textContent = `Fecha Fin: ${practica.fin}`;
+                detailsContainer.appendChild(periodoLabel);
+
+                const empresaLabel = document.createElement('p');
+                empresaLabel.textContent = `Empresa: ${practica.empresa.nombre}`;
+                detailsContainer.appendChild(empresaLabel);
+
+                const docenteLabel = document.createElement('p');
+                docenteLabel.textContent = `Docente: ${practica.docente.nombre} ${practica.docente.apellido}`;
+                detailsContainer.appendChild(docenteLabel);
+
+                const numIdentificacionLabel = document.createElement('p');
+                numIdentificacionLabel.textContent = `Num Identificacion: ${practica.docente.numIdentificacion}`;
+                detailsContainer.appendChild(numIdentificacionLabel);
+
+                detailsContainer.appendChild(document.createElement('p'));
+            });
+        } else {
+            const errorLabel = document.createElement('p');
+            errorLabel.textContent = 'No se encontraron prácticas para el docente especificado.';
+            detailsContainer.appendChild(errorLabel);
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        const detailsContainer = document.getElementById('detailsContainer');
+        detailsContainer.innerHTML = 'Error al obtener los datos. Por favor, inténtelo de nuevo más tarde.';
+    });
+}
 
 
 
